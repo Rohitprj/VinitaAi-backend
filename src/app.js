38d742ac077089ce35a30ddabf02dc653235fafd
Rohit kumar <147251684+Rohitprj@users.noreply.github.com>
2025-10-26 19:45:23 +0530

    Update CORS origin to include localhost:3000

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3001",
+      "http://localhost:3000",
       "http://localhost:3002",
       "https://d5cnvsgq-3001.inc1.devtunnels.ms",
       "https://chat-bot-umber-rho.vercel.app",
