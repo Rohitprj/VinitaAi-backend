9e7b5f21cdb55386e1a2662fc5bf67d4352bb8c8
Rohit kumar <rohitprj.1282@gmail.com>
2025-10-26 12:37:17 +0530

    fix: Remove duplicate request origin logging middleware

--- src/app.js
+++ src/app.js
@@ -18,6 +18,11 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  console.log("Request Origin:", req.headers.origin);
+  next();
+});
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
