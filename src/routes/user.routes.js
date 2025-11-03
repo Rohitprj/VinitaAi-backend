30285278852b17e08d2df130fa148d6a9b4cfa97
mk9405 <git config --global user.email>
2025-11-03 04:33:30 -0800

    added new routes getallusers and getuserbyid

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import {
+  getAllUsers,
+  getUserById,
   login,
   logout,
   register,
@@ -13,5 +15,7 @@ router.post("/register", register);
 router.get("/verify/:token", verifyEmail);
 router.post("/login", login);
 router.post("/logout", requireAuth, logout);
+router.get("/", requireAuth, getAllUsers);
+router.get("/:userId", requireAuth, getUserById);
 
 export default router;
