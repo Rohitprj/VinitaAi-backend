cec4e60b8d888f6a9d556aaf74b6509f211b162a
Rohit kumar <rohitprj.1282@gmail.com>
2025-11-06 18:43:45 +0530

    feat: Implement admin authentication middleware and update admin routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { requireAuth } from "../middlewares/authMiddleware.js";
 import { getAllUsers, getUserById } from "../controllers/admin.controller.js";
+import { adminAuth } from "../middlewares/adminMiddleware.js";
 
 const router = Router();
 
 // Admin routes
-router.get("/", requireAuth, getAllUsers);
-router.get("/:userId", requireAuth, getUserById);
+router.get("/", adminAuth, getAllUsers);
+router.get("/:userId", adminAuth, getUserById);
 
 export default router;
