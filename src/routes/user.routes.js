046429cfdc2504df07e0115606d136df8d984cb5
Rohit kumar <rohitprj.1282@gmail.com>
2025-11-07 13:28:37 +0530

    feat: Add login and logout routes to admin and user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,13 +5,12 @@ import {
   register,
   verifyEmail,
 } from "../controllers/user.controller.js";
-import { requireAuth } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.get("/verify/:token", verifyEmail);
 router.post("/login", login);
-router.post("/logout", requireAuth, logout);
+router.post("/logout", logout);
 
 export default router;
